c5a797501fdbea75e06b488d870cfc50cf87d96e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-19 03:42:55 +0000

    Deploy rafonsosilva/week05_comp3104_lab_exec to github.com/rafonsosilva/week05_comp3104_lab_exec.git:gh-pages

---
+++ precache-manifest.50ca8f34b5e658ef243d676940a696cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0c55de29e47b99c5e2f6",
+    "url": "/static/css/main.53109f7d.chunk.css"
+  },
+  {
+    "revision": "0c55de29e47b99c5e2f6",
+    "url": "/static/js/main.0c55de29.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "206892ec38c99bdd1291",
+    "url": "/static/js/2.206892ec.chunk.js"
+  },
+  {
+    "revision": "18fd3625f2a667a39ba5790e86575d94",
+    "url": "/index.html"
+  }
+];
